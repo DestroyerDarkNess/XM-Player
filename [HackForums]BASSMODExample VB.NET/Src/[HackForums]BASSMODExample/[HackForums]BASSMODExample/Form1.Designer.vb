85bd7b5747f36600faaa5ce05831fa18de64f18e
S4Lsalsoft <grupo.fisica.638@gmail.com>
2019-10-30 13:04:28 -0430

    no message

---
+++ [HackForums]BASSMODExample VB.NET/Src/[HackForums]BASSMODExample/[HackForums]BASSMODExample/Form1.Designer.vb
@@ -0,0 +1,121 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackgroundImage = CType(resources.GetObject("Button1.BackgroundImage"), System.Drawing.Image)
+        Me.Button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Button1.FlatAppearance.BorderColor = System.Drawing.Color.SpringGreen
+        Me.Button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Silver
+        Me.Button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gray
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Location = New System.Drawing.Point(561, 11)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(22, 18)
+        Me.Button1.TabIndex = 0
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox1.Location = New System.Drawing.Point(61, 14)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(494, 13)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Consolas", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(9, 12)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(56, 15)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Music: "
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.SpringGreen
+        Me.Panel1.Controls.Add(Me.TextBox1)
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Location = New System.Drawing.Point(0, 12)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(590, 45)
+        Me.Panel1.TabIndex = 3
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(12, 87)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(59, 37)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Play"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(77, 87)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(59, 37)
+        Me.Button3.TabIndex = 5
+        Me.Button3.Text = "Pause"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(620, 152)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Form1"
+        Me.Text = "XM PLAYER By **Aincrad**"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+
+End Class
---
+++ [HackForums]BASSMODExample VB.NET/Src/[HackForums]BASSMODExample/[HackForums]BASSMODExample/Form1.vb
@@ -0,0 +1,81 @@
+﻿Imports System.IO
+Imports Un4seen.BassMOD.BassMOD
+
+Public Class Form1
+
+    Dim XmFileData As Byte() = Nothing
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+        'stoped dll events, for closed app
+        BASSMOD_MusicPause()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+            Dim Inicializacion As Boolean = BASSMOD_Init(-1, 44100, 0)
+
+            If Inicializacion = False Then
+                MsgBox("Error; Failed to Start BassMod")
+                End
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim DirXMfile As String = openFile()
+        If Not DirXMfile = "Error" Then
+            TextBox1.Text = DirXMfile
+            XmFileData = File.ReadAllBytes(DirXMfile)
+        End If
+    End Sub
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If Not TextBox1.Text = "" Then
+            BASSMOD_MusicLoad(XmFileData, 0, CUInt(XmFileData.Length), BASSMOD_CONSTANTS.BASS_MUSIC_LOOP Or BASSMOD_CONSTANTS.BASS_MUSIC_RAMPS Or BASSMOD_CONSTANTS.BASS_MUSIC_SURROUND Or BASSMOD_CONSTANTS.BASS_MUSIC_CALCLEN)
+            BASSMOD_MusicPlay()
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        BASSMOD_MusicPause()
+    End Sub
+
+
+#Region " BassMod Constanst Structure "
+
+    Friend Structure BASSMOD_CONSTANTS
+        Public Const BASS_MUSIC_LOOP As Integer = 4
+        Public Const BASS_MUSIC_RAMPS As Integer = 2
+        Public Const BASS_MUSIC_SURROUND As Integer = 512
+        Public Const BASS_MUSIC_CALCLEN As Integer = 8192
+        Public Const BASS_SYNC_END As Integer = 2
+    End Structure
+
+#End Region
+
+#Region " Funcs "
+
+    Public Shared Function openFile(Optional ByVal Custom_Filter As String = "Executables (*.xm)|*.xm") As String
+        Using ofd As New OpenFileDialog
+            With ofd
+                .AddExtension = True
+                .AutoUpgradeEnabled = True
+                .CheckPathExists = True
+                .Title = "Selec File"
+                .Filter = Custom_Filter
+                .FileName = ""
+                .RestoreDirectory = True
+            End With
+            If ofd.ShowDialog() = DialogResult.OK Then
+                Return ofd.FileName
+            End If
+        End Using
+        Return "Error"
+    End Function
+
+#End Region
+
+End Class
